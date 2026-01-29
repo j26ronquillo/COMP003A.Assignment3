f30100914b76711bc087bf78093f26d8d90a8fd1
joshalvinronquillo <j26ronquillo@gmail.com>
2026-01-28 19:45:37 -0800

    created the time-based readiness status user input and calculation

---
+++ COMP003A.Assignment3/COMP003A.Assignment3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Transactions;
+
+namespace COMP003A.Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Academic & Personal Readiness Evaluator ===\n");
+            Console.WriteLine($"Current Year: {DateTime.Now.Year}");
+            Console.Write("Enter your birth year: ");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Calculated Age: {2026 - birthYear}");
+        }
+    }
+}
