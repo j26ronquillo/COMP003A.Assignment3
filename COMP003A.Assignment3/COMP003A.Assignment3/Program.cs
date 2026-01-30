d2fe7d7ee4b61dc4bcef620f53d9abe11a8a6742
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-01-30 13:39:38 -0800

    created the guidance option titles/numbers and the 3 differnt cases

--- COMP003A.Assignment3/COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/COMP003A.Assignment3/Program.cs
@@ -8,17 +8,51 @@ namespace COMP003A.Assignment3
         {
             Console.WriteLine("=== Academic & Personal Readiness Evaluator ===\n");
             Console.WriteLine($"Current Year: {DateTime.Now.Year}");
-            Console.Write("Enter your birth year: ");
+            Console.Write($"Enter your birth year: {birthYear}\n");
             int birthYear = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"Calculated Age: {DateTime.Now.Year - birthYear}");
+            int calcBirthYear = (DateTime.Now.Year - birthYear);
 
-            if (birthYear >= 0 && birthYear <= 14)
-            { Console.WriteLine("Child"); }
-            else if (birthYear >= 15 && birthYear <= 64)
-            { Console.WriteLine("Adult"); }
-            else if (birthYear >= 65)
-            { Console.WriteLine("Elder"); }
+            if (calcBirthYear >= 0 && calcBirthYear <= 17)
+            { Console.WriteLine("Readiness Category: Child\n"); }
+            else if (calcBirthYear >= 18 && calcBirthYear <= 64)
+            { Console.WriteLine("Readiness Category: Adult\n"); }
+            else if (calcBirthYear >= 65)
+            { Console.WriteLine("Readiness Category: Elder\n"); }
+
+            Console.Write("Do you have a valid ID? (yes/no): ");
+            string validID = Console.ReadLine();
+            bool idTrue = (validID == "yes");
+
+            Console.Write("Have you completed orientation? (yes/no): ");
+            string completeOri = Console.ReadLine();
+            bool passedOri = (completeOri == "yes");
+
+            if (calcBirthYear >= 18)
+            {
+                if (idTrue && passedOri)
+                { Console.WriteLine("Readiness Status: Ready!"); }
+                else if (idTrue || passedOri)
+                { Console.WriteLine("Readiness Status: Conditionally Ready."); }
+                else
+                { Console.WriteLine("Readiness Status: Not Ready..."); }
+
+                Console.WriteLine("\nSelect a guidance option (1/2/3): ");
+                Console.WriteLine("1 - Academic Planning");
+                Console.WriteLine("2 - Personal Planning");
+                Console.WriteLine("3 - Career Planning");
+                Console.Write("Enter choice: ");
+                int guidanceOption = int.Parse(Console.ReadLine());
+
+                switch (guidanceOption)
+                {       case 1: Console.WriteLine("\nGuidance: \nTalk to an academic counselor to plan your next term."); break;
+                        case 2: Console.WriteLine("\nGuidance: \nCreate a personal schedule with a teacher to balance school and life."); break;
+                        case 3: Console.WriteLine("\nGuidance: \nFind a career project manager to find career options and internships realted to your major."); break;
+                        default: Console.WriteLine("\nGuidance: \nERROR: Invalid guidance option..."); break;
+                }
+
+            }
         }
     }
 }
